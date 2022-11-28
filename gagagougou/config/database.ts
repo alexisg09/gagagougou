9915d4111c3cd7e55b3135cb19e60e35ae897690
LRAV-Alexis <alexis.grenier@lrav.fr>
2022-11-28 16:57:59 +0100

    prout

--- gagagougou/config/database.ts
+++ gagagougou/config/database.ts
@@ -1,7 +1,6 @@
 export default ({ env }) => ({
   connection: {
     client: 'postgres',
-    
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
